756f743009b4d22702b816e75608876ec3ac9072
HendrikMeyer <hendrik.meyer@sap.com>
2024-02-23 12:21:39 +0200

    Update api.py

--- api.py
+++ api.py
@@ -258,6 +258,3 @@ async def createPicture(author, width, heigth, description, picture_id):
         l[picture_id] = new_picture
     with open("./pictures.json", "w") as t:
         json.dump(l,t,indent=4)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
